c5a65b5aab1707d9ca3d1bd5a527e5544f8f6d8c
unknown <shubhamdave70@gmail.com>
2022-04-12 12:44:39 -0400

    Added delete all history feature

--- serverSide.js
+++ serverSide.js
@@ -1,44 +1,55 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ShortUrl = require('./models/shortUrl')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const app = express();
 
-mongoose.connect('mongodb://localhost/urlShortener', {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
-})
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
 
-app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
 
-  res.redirect('/')
-})
+  res.redirect("/");
+});
 
-app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
 
-  shortUrl.clicks++
-  shortUrl.save()
+  shortUrl.clicks++;
+  shortUrl.save();
 
-  res.redirect(shortUrl.full)
-})
+  res.redirect(shortUrl.full);
+});
 
-app.get('/delete/shortUrl/:_id', async (req, res) => {
-    const { _id } = req.params;
-    ShortUrl.deleteOne({ _id })
+app.get("/delete/shortUrl/:_id", async (req, res) => {
+  const { _id } = req.params;
+  ShortUrl.deleteOne({ _id })
     .then(() => {
-        console.log("Deleted successfully");
-        res.redirect('/');
+      console.log("Deleted successfully");
+      res.redirect("/");
     })
-    .catch((err) => console.log(err)); 
-})
+    .catch((err) => console.log(err));
+});
+
+app.delete("/Delete_Data", async (request, res) => {
+  ShortUrl.deleteMany({}, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
 
 app.listen(process.env.PORT || 5000);
